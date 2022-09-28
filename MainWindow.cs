6b62ef550ef7341cd6baf9450d0755d1dd60fb8e
Bartiszny <29127947+Bartiszny@users.noreply.github.com>
2022-09-28 20:42:28 +0200

    Initial commit with working game window
    Game window has now buttons with emojis.

---
+++ MainWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        SetUpGame();
+    }
+
+    private void SetUpGame()
+    {
+        List<string> animalEmoji = new List<string>()
+        {
+            "🐶","🐶",
+            "🐱","🐱",
+            "🐭","🐭",
+            "🐰","🐰",
+            "🦊","🦊",
+            "🐼","🐼",
+            "🐨","🐨",
+            "🐯","🐯",
+        };
+        Random random = new Random();
+        foreach (Gtk.Button button in table2.AllChildren)
+        {
+            int index = random.Next(animalEmoji.Count);
+            string nextEmoji = animalEmoji[index];
+            button.Label = nextEmoji;
+            animalEmoji.RemoveAt(index);
+        }
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnButtonClicked(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        Console.WriteLine($"Button {button.Name} clicked!");
+    }
+}
